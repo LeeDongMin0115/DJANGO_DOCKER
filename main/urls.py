37d396147cad371b03cd7bb89a9a250fb56e528c
LeeDongMin0115 <night7501@naver.com>
2026-04-28 11:49:57 +0900

    Update allowed hosts

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path 
 from . import views
+from .views import post_list, create_post, delete_post
 
 urlpatterns = [
-    path("", views.hello_view, name="hello"),
+    path('post_list', post_list, name='post_list'),
+    path('create', create_post, name='create_post'),
+    path('delete/<int:post_id>/', delete_post, name='delete_post'),
 ]
